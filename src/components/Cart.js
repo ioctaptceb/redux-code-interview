4f80febd3c020f0f335d9ba63ab3714b6c6d5695
ioctaptceb <yulia.startsev@gmail.com>
2017-03-10 11:54:27 +0100

    add code review exercise

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,38 +1,38 @@
 import React, { PropTypes } from 'react'
 import Product from './Product'
 
-const Cart  = ({ products, total, onCheckoutClicked }) => {
-  const hasProducts = products.length > 0
-  const nodes = hasProducts ? (
-    products.map(product =>
-      <Product
-        title={product.title}
-        price={product.price}
-        quantity={product.quantity}
-        key={product.id}
-      />
+class Cart extends React.Component {
+  constructor(props) {
+    super();
+    this.props = props
+    this.state = {
+      hasProducts: props.products.length > 0
+    }
+  }
+
+  render() {
+    let nodes;
+    if (this.state.hasProducts) {
+     nodes = this.props.products.map(product =>
+        <Product
+          title={product.title}
+          price={product.price}
+          quantity={product.quantity}
+        />
+      )
+    } else {
+      nodes = <em>Please add some products to cart.</em>
+    }
+
+    return (
+      <div>
+        <h3>Your Cart</h3>
+        <div>{nodes}</div>
+        <p>Total: &#36;{this.props.total}</p>
+        <button onClick={this.props.onCheckoutClicked} disabled={this.state.hasProducts ? '' : 'disabled'}> Checkout </button>
+      </div>
     )
-  ) : (
-    <em>Please add some products to cart.</em>
-  )
-
-  return (
-    <div>
-      <h3>Your Cart</h3>
-      <div>{nodes}</div>
-      <p>Total: &#36;{total}</p>
-      <button onClick={onCheckoutClicked}
-        disabled={hasProducts ? '' : 'disabled'}>
-        Checkout
-      </button>
-    </div>
-  )
-}
-
-Cart.propTypes = {
-  products: PropTypes.array,
-  total: PropTypes.string,
-  onCheckoutClicked: PropTypes.func
+  }
 }
 
 export default Cart
--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -10,15 +10,10 @@ const initialState = {
 }
 
 const addedIds = (state = initialState.addedIds, action) => {
-  switch (action.type) {
-    case ADD_TO_CART:
-      if (state.indexOf(action.productId) !== -1) {
-        return state
-      }
-      return [ ...state, action.productId ]
-    default:
-      return state
+  if (action.type === ADD_TO_CART) {
+      state.push(action.productId);
   }
+  return state
 }
 
 const quantityById = (state = initialState.quantityById, action) => {
